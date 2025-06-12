3dde27881e2848f9c00ba95d45de266d2b4331d8
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-06-12 16:46:25 -0500

    Add initial view functions for hello and about pages

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def hello(request):
+  return HttpResponse("Hello, world! This is my Django app.")
+
+def about(request):
+  return HttpResponse("This is the about page of my Django app.")
